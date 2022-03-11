9cdc1490069a216ef5c657fc981ba364f8c22820
Francisco Marfull <marfullsen@gmail.com>
2022-03-11 06:14:55 -0300

    deploy

---
+++ precache-manifest.084362412aea04f3dd1849dcae403a03.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3fece7925a697334fc9",
+    "url": "/estilos-sociales/css/app.82678263.css"
+  },
+  {
+    "revision": "c95b6f9cd5bf4763f924",
+    "url": "/estilos-sociales/css/chunk-vendors.99658ae2.css"
+  },
+  {
+    "revision": "1a2f6ffbb79bf108984fddb05c8e32f1",
+    "url": "/estilos-sociales/img/cartesianSocialStyle.1a2f6ffb.png"
+  },
+  {
+    "revision": "aa407bb2d9589cfb34a76d705e9c8119",
+    "url": "/estilos-sociales/index.html"
+  },
+  {
+    "revision": "b3fece7925a697334fc9",
+    "url": "/estilos-sociales/js/app.c7f254e0.js"
+  },
+  {
+    "revision": "c95b6f9cd5bf4763f924",
+    "url": "/estilos-sociales/js/chunk-vendors.43e08e46.js"
+  },
+  {
+    "revision": "0f81b5db5fc829151abcbc9793b69328",
+    "url": "/estilos-sociales/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/estilos-sociales/robots.txt"
+  }
+]);
